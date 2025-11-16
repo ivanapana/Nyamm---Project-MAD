e41de4e55955bd94a17f6e06ef2f9e4471c08cd6
Marcovan Pieter <s22310444@student.unklab.ac.id>
2025-11-16 00:55:23 -1200

    Final Profile

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,45 @@
+// src/pages/SignIn/index.tsx
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+
+const SignIn = ({navigation}) => (
+  <View style={styles.page}>
+    <Text style={styles.text}>Sign In Screen</Text>
+
+    <TouchableOpacity
+      style={styles.button}
+      onPress={() => navigation.navigate('Main')}>
+      {' '}
+      {/* <-- ke BottomTabNavigator */}
+      <Text style={styles.buttonText}>Masuk ke Dashboard</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+export default SignIn;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 32,
+  },
+  button: {
+    backgroundColor: '#FBBF24',
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    borderRadius: 12,
+    elevation: 3,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#000',
+  },
+});
