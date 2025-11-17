8a3f3fc6a88c0f52bc9ea6386af4589c34e2c1cd
lioilio <orohinjilio588@gmail.com>
2025-11-18 00:45:34 +0800

    save local changes

--- App.tsx
+++ App.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// File: App.tsx
 
-// Impor semua layar Anda
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import GetStarted from './src/pages/GetStarted';
 import SignIn from './src/pages/SignIn';
 import SignUp from './src/pages/SignUp';
 import Dashboard from './src/pages/Dashboard';
 import PerencanaMenu from './src/pages/PerencanaMenu';
+import Bahan from './src/pages/Bahan';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        screenOptions={{headerShown: false}} 
-        initialRouteName="Get"
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Bahan"
       >
         <Stack.Screen name="GetStarted" component={GetStarted} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
         <Stack.Screen name="PerencanaMenu" component={PerencanaMenu} />
-        
-       
+        <Stack.Screen name="Bahan" component={Bahan} />
       </Stack.Navigator>
     </NavigationContainer>
   );
