2c4bda7bd646ee70d2805bd267263104be47dd8a
ivanapana <s22310210@student.unklab.ac.id>
2025-11-23 17:59:47 +0800

    Implementasi fitur Daftar Belanja (Firebase) dan perbaikan struktur Navigasi

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
+// App.tsx
+
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import FlashMessage from 'react-native-flash-message';
+import './src/config/Firebase';
 
-import KumpulanResep from './src/pages/KumpulanResep';
 import GetStarted from './src/pages/GetStarted';
 import SignIn from './src/pages/SignIn';
 import SignUp from './src/pages/SignUp';
-import {BottomTabNavigator} from './src/components/organisms/BottomNavbar/BottomTabNavigator';
 import Profile from './src/pages/Profile';
 import Detail from './src/pages/Detail';
-import Rencana from './src/pages/Rencana';
-import './src/config/Firebase';
+import KumpulanResep from './src/pages/KumpulanResep';
+
+import {BottomTabNavigator} from './src/components/organisms/BottomNavbar/BottomTabNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,10 +26,9 @@ export default function App() {
         <Stack.Screen name="GetStarted" component={GetStarted} />
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="Main" component={BottomTabNavigator} />
-        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="MainApp" component={BottomTabNavigator} />
         <Stack.Screen name="Detail" component={Detail} />
-        <Stack.Screen name="Rencana" component={Rencana} />
+        <Stack.Screen name="Profile" component={Profile} />
         <Stack.Screen name="KumpulanResep" component={KumpulanResep} />
       </Stack.Navigator>
       <FlashMessage position="top" />
